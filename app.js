42379d965fb865fab3d35080114f03a478c3c03d
Kenan Baylan <kenanbaylan5434@gmail.com>
2022-08-13 17:38:51 +0300

    front fields added and backend starting

--- app.js
+++ app.js
@@ -1,3 +1,13 @@
-const fn = function () {
-  console.log("Hello World");
-};
+const express = require("express");
+const path = require("path");
+const app = express();
+
+//middleware
+app.use(express.static("public"));
+
+app.get("/", (req, res, next) => {
+  res.sendFile(path.resolve(__dirname, "temp/index.html"));
+});
+
+const port = 3000;
+app.listen(port, () => console.log(`Example app listening on port ${port}!`));
