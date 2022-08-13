0938f16ef1e2492e2bdb992b9f4e3d702e338604
Kenan Baylan <kenanbaylan5434@gmail.com>
2022-08-13 23:36:46 +0300

    pcat  front template on the backend starting.

--- app.js
+++ app.js
@@ -1,12 +1,30 @@
 const express = require("express");
 const path = require("path");
+const ejs = require("ejs");
+
 const app = express();
+
+//template engine setup
+app.set("view engine", "ejs"); //set the view engine to ejs , used ejs for templating
 
 //middleware
 app.use(express.static("public"));
 
+//routes
 app.get("/", (req, res, next) => {
-  res.sendFile(path.resolve(__dirname, "temp/index.html"));
+  res.render("index"); //first render the index.ejs file
+});
+
+app.get("/about", (req, res, next) => {
+  res.render("about"); //first render the index.ejs file
+});
+
+app.get("/addPhoto", (req, res, next) => {
+  res.render("addPhoto"); //first render the index.ejs file
+});
+
+app.post("/photos", (req, res, next) => {
+  console.log(req.body);
 });
 
 const port = 3000;
